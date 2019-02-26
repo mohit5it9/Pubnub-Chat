f0c9e1b6041564811955af9e67a4b5ed5c15cbba
Mohit Marwari <mmarwadi@gmail.com>
2019-02-25 23:29:15 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var pubnub = require("chat-engine");
+var typing = require('chat-engine-typing-indicator');
+
+app.use(express.static(__dirname + '/public'));
+
+
+var connectedUsers = 0;
+
+io.on('connection', function(socket){
+	connectedUsers++;
+	io.emit('chat connections',connectedUsers);
+	socket.on('disconnect',function(){
+		connectedUsers--;
+		io.emit('chat connections',connectedUsers);
+	});
+});
+
+http.listen(3000, function(){
+  console.log('listening on * : 3000');
+});
